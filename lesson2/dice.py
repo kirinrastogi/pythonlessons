57caa86697697bf72c493de57bff418a186d65f7
Kirin <kirin.rastogi@shopify.com>
2024-04-20 00:10:39 -0400

    add dice base file

---
+++ lesson2/dice.py
@@ -0,0 +1,7 @@
+print("What is your guess?: ")
+guess = input()
+
+print("You guessed: " + guess)
+
+# look at the package "random" documentation: https://docs.python.org/3/library/random.html
+# let's import this module and try and use a function from it to make a dice guessing game
